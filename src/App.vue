ef94dc7cd87be73c8d60e99f0f60c8a765ac5fb9
PJ Trainor <trainorpj@gmail.com>
2018-02-27 21:22:25 -0500

    firebase works with my app!  🎉🎊

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="app" class="flex flex-col h-screen justify-between">
-    <header class="bg-pink-light text-white p-6">
-      <h2>{{currentMessage}}</h2>
+    <header class="bg-green-dark text-white p-6">
+      <h2>Let the games begin...</h2>
     </header>
     <buttons-holder 
       class="flex md:flex-row flex-col flex-1" 
       :buttons="bestButtons">
       <li 
-        slot="im-the-best" slot-scope="{count, name, id, isBest}"
-        :class="`list-reset flex-1 p-3 md:p-6 bg-${name}-light flex flex-row md:flex-col justify-around items-center`">
+        slot="im-the-best" slot-scope="{value, key, id, isBest}"
+        :class="`list-reset flex-1 p-3 md:p-6 bg-${key}-light flex flex-row md:flex-col justify-around items-center`">
           <h3 :class="`text-white self-center ${isBest ? 'visible' : 'invisible'}`">This is the best button</h3>
           <button 
-            :class="`bg-transparent hover:bg-white text-white font-semibold hover:text-${name}-dark py-2 px-4 border border-white hover:border-transparent rounded-full h-16 w-16 md:h-32 md:w-32 flex items-center justify-center`"
-            @click="addCount(id)">
-            {{count}}
+            :class="`bg-transparent hover:bg-white text-white font-semibold hover:text-${key}-dark py-2 px-4 border border-white hover:border-transparent rounded-full h-16 w-16 md:h-32 md:w-32 flex items-center justify-center`"
+            @click="addCount({id, key})">
+            {{value}}
           </button>
       </li>
     </buttons-holder>
-    <footer class="bg-pink-light text-white p-6">
+    <footer class="bg-green-dark text-white p-6">
       <h2>There are {{total}} votes</h2>
       </footer>
   </div>
@@ -37,17 +37,15 @@ export default {
     "buttons-holder": ButtonsHolder
   },
   firebase: {
-    firebuttons: db.ref("buttons")
+    buttons: {
+      source: db.ref("buttons"),
+      cancelCallback(err) {
+        console.error(err);
+      }
+    }
   },
   data() {
     return {
-      bestIndex: 0,
-      buttons: [
-        { count: 0, name: "blue" },
-        { count: 0, name: "red" },
-        { count: 0, name: "purple" },
-        { count: 0, name: "orange" }
-      ],
       messages: [
         "No wait...",
         "Actually, you know what...",
@@ -56,24 +54,30 @@ export default {
     };
   },
   computed: {
+    maxCount() {
+      return Math.max(...this.buttons.map(d => d[".value"]));
+    },
     bestButtons() {
       return this.buttons.map((b, i) => ({
-        ...b,
-        isBest: i === this.bestIndex,
+        key: b[".key"],
+        value: +b[".value"],
+        isBest: b[".value"] >= this.maxCount,
         id: i
       }));
     },
     total() {
-      return this.buttons.reduce((acc, curr) => acc + curr.count, 0);
+      return this.bestButtons.reduce((acc, curr) => acc + curr.value, 0);
     },
     currentMessage() {
       return this.messages[this.total % this.messages.length];
     }
   },
   methods: {
-    addCount(id) {
-      this.buttons[id].count++;
-      this.bestIndex = id;
+    addCount({ id, key }) {
+      const value = +this.bestButtons[id].value + 1;
+      console.log(value);
+      this.$firebaseRefs.buttons.child(key).set(value);
+      // this.$firebaseRefs.buttons.child(key);
     }
   }
 };
